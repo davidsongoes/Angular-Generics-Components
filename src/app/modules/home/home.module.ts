6fb38cd3e0cffaf3549583baf2d3cb8f40ecbcd9
Davidson Góes <davidsongoes@gmail.com>
2022-03-14 19:57:02 -0300

    feat(card): :lipstick: Implementa elemento de layout

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,17 +1,12 @@
+import { MaterialModule } from './../../shared/material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeIndexComponent } from './container/home-index/home-index.component';
 
-
 @NgModule({
-  declarations: [
-    HomeIndexComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeIndexComponent],
+  imports: [CommonModule, HomeRoutingModule, MaterialModule],
 })
-export class HomeModule { }
+export class HomeModule {}
